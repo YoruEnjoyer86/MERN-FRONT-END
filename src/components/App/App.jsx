91c24d6c1eff2c1ad06a0ccaea18b5fc2c2d6f46
YoruEnjoyer86 <bibartalexandruixc@gmail.com>
2024-05-22 22:14:40 +0300

    navbarul acum este tot timpul vizibil cand dai scroll, adaugat searchbar

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { AppContext } from "../../Contexts/AppContext";
+import "./App.css";
 
 const App = ({ children }) => {
   const [cartNotifications, setCartNotifications] = useState(0);
@@ -7,6 +8,7 @@ const App = ({ children }) => {
   console.log("SADAS");
   return (
     <AppContext.Provider value={cartNotifications}>
+      <span className="space_for_navBar"></span>
       <div>{children}</div>
     </AppContext.Provider>
   );
--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -1,12 +1,8 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "./SearchBar.css";
+import axios from "axios";
 
 let text = "";
-
-const HandleOnChange = (event) => {
-  text = event.target.value;
-  //console.log(text) ;
-};
 
 const HandleOnKeyUp = (event) => {
   //console.log(event.key) ;
@@ -17,21 +13,59 @@ const HandleSearch = () => {
   alert("Searching for " + text);
 };
 
-const SearchBar = ({ style, className }) => {
+const SearchBar = ({ className }) => {
+  const [areSearchResultsVisible, setSearchResultsVisible] = useState(false);
+  const [searchResults, setSearchResults] = useState([]);
+
+  const HandleOnChange = async (event) => {
+    text = event.target.value;
+    if (text.length != 0) {
+      setSearchResultsVisible(true);
+      let res = await axios.post("http://localhost:3001/get_search_results", {
+        text,
+      });
+      setSearchResults(res.data.results);
+      console.log(res.data.results);
+    } else setSearchResultsVisible(false);
+    // console.log(text.length);
+  };
+
+  useEffect(() => {
+    console.log(searchResults);
+  }, [searchResults]);
   return (
-    <div className={"main_search_bar " + className} style={{ ...style }}>
-      <input
-        className="input_search_bar"
-        type="text"
-        onChange={() => {
-          HandleOnChange(event);
+    <>
+      <div
+        className="invisible_click_handler"
+        onClick={() => {
+          setSearchResultsVisible(false);
         }}
-        onKeyUp={() => {
-          HandleOnKeyUp(event);
-        }}
-        placeholder="Search something!"
-      />
-    </div>
+      ></div>
+      <div className={"main_search_bar " + className}>
+        <input
+          id="search_bar_input"
+          className="input_search_bar"
+          type="text"
+          onFocus={() => {
+            setSearchResultsVisible(true);
+          }}
+          onChange={() => {
+            HandleOnChange(event);
+          }}
+          onKeyUp={() => {
+            HandleOnKeyUp(event);
+          }}
+          placeholder="Search something!"
+        />
+        {areSearchResultsVisible && (
+          <div className="search_results_column">
+            {searchResults.map((res) => (
+              <p className="result_search_bar_text">{res.name}</p>
+            ))}
+          </div>
+        )}
+      </div>
+    </>
   );
 };
 
