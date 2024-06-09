24217cae899e0a58531d20314df413510620055f
YoruEnjoyer86 <bibartalexandruixc@gmail.com>
2024-06-10 02:05:49 +0300

    schimbat url backend in render.com

--- src/base_url.js
+++ src/base_url.js
@@ -1,6 +1,5 @@
 let local_url = "http://localhost:3001";
-let deployment_url =
-  "https://ecommerce-bibart-alexandru-backend.onrender.com:3001";
+let deployment_url = "https://ecommerce-bibart-alexandru-backend.onrender.com";
 let base_url = "";
 
 if (process.env.NODE_ENV === "production") base_url = deployment_url;
